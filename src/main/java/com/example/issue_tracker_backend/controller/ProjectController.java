bd8934e4454df9d459ef0ec4897e6abd83766458
Toshko Batsanov <totobackn@gmail.com>
2023-06-12 14:50:23 +0300

    Implemented REST Controllers and Services for required frontend requests

--- src/main/java/com/example/issue_tracker_backend/controller/ProjectController.java
+++ src/main/java/com/example/issue_tracker_backend/controller/ProjectController.java
@@ -2,8 +2,11 @@ package com.example.issue_tracker_backend.controller;
 
 import com.example.issue_tracker_backend.conversions.TicketConversion;
 import com.example.issue_tracker_backend.dtos.TicketDto;
+import com.example.issue_tracker_backend.model.Project;
 import com.example.issue_tracker_backend.model.Ticket;
+import com.example.issue_tracker_backend.service.ProjectService;
 import com.example.issue_tracker_backend.service.TicketService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,23 +18,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/private/api/projects")
+@RequestMapping()
 public class ProjectController {
 
     private final TicketService ticketService;
+    private final ProjectService projectService;
 
-    public ProjectController(final TicketService ticketService) {
+    @Autowired
+    public ProjectController(final TicketService ticketService, ProjectService projectService) {
         this.ticketService = ticketService;
+        this.projectService = projectService;
     }
 
-    @GetMapping("issues/{id}")
+    @GetMapping("private/api/projects/issues/{id}")
     public ResponseEntity<List<TicketDto>> getTickets(@PathVariable("id") Long id) {
-        List<Ticket> tickets=ticketService.findAllTicketsByProject(id);
+        List<Ticket> tickets = ticketService.findAllTicketsByProject(id);
         ArrayList<TicketDto> ticketsDto = new ArrayList<TicketDto>();
         ticketsDto.ensureCapacity(tickets.size());
         for (Ticket ticket : tickets) {
             ticketsDto.add(TicketConversion.entityToDto(ticket));
         }
         return new ResponseEntity<>(ticketsDto, HttpStatus.OK);
+    }
+
+    @GetMapping("projects/all")
+    ResponseEntity<List<Project>> getAllProjects() {
+        return new ResponseEntity<>(projectService.getAllProjects(), HttpStatus.OK);
     }
 }
