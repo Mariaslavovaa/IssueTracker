916d7f59b1267a379ba3aa53d11b9008be511ad8
Toshko Batsanov <totobackn@gmail.com>
2023-06-12 09:36:38 +0300

    Removed @Query in UserRepository as queries are automatically created based on function name, according to Spring documentation

--- src/main/java/com/example/issue_tracker_backend/repository/UserRepository.java
+++ src/main/java/com/example/issue_tracker_backend/repository/UserRepository.java
@@ -1,17 +1,12 @@
 package com.example.issue_tracker_backend.repository;
 
 import com.example.issue_tracker_backend.model.User;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
-    @Query("SELECT u FROM User u WHERE u.username = ?1")
     User findByUsername(String username);
-    @Query("SELECT u FROM User u WHERE u.email = ?1")
     User findByEmail(String email);
-
 }
