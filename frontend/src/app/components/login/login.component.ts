56089a2fa9e71b0037e3775c065584df44946d38
Mariaslavovaa <mimi161514@gmail.com>
2023-06-15 22:21:37 +0300

    fix on login component

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -11,7 +11,6 @@ import { LoginService } from 'src/app/service/login.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-
 export class LoginComponent implements OnInit{
 
   form: any = {
