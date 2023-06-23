0a1e72e87ae7224e10d4c9a94fe9787d496f9365
bozhidarak <bgkbgk6@gmail.com>
2023-06-23 09:03:04 +0300

    remove throwing an unhandled exception

--- frontend/src/app/service/token-service.service.ts
+++ frontend/src/app/service/token-service.service.ts
@@ -1,33 +1,35 @@
-import { Injectable } from "@angular/core"
-
-@Injectable({ providedIn: "root" })
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+@Injectable({ providedIn: 'root' })
 export class TokenStorageService {
-    constructor() {}
+  constructor(private router: Router) {}
 
-    signOut(): void {
-        window.sessionStorage.clear();
+  signOut(): void {
+    window.sessionStorage.clear();
+  }
+
+  public saveToken(token: string): void {
+    window.sessionStorage.removeItem('SESSION');
+    window.sessionStorage.setItem('SESSION', token);
+  }
+
+  public getToken(): string | null {
+    return window.sessionStorage.getItem('SESSION');
+  }
+
+  public getUsername(): string {
+    const user = window.sessionStorage.getItem('USER');
+    if (user != null) {
+      const userObject = JSON.parse(user);
+      return userObject.username;
     }
 
-    public saveToken(token: string): void {
-        window.sessionStorage.removeItem("SESSION");
-        window.sessionStorage.setItem("SESSION", token);
-    }
+    this.router.navigateByUrl('/signup');
+    return '';
+  }
 
-    public getToken(): string | null {
-        return window.sessionStorage.getItem("SESSION");
-    }
-
-    public getUsername(): string{
-        const user = window.sessionStorage.getItem("USER");
-        if (user != null) {
-            const userObject = JSON.parse(user);
-            return userObject.username;
-        }
-        throw new Error("Not logged in")
-    }
-
-    public saveUser(user: any): void {
-        window.sessionStorage.removeItem("USER");
-        window.sessionStorage.setItem("USER", JSON.stringify(user));
-    }
+  public saveUser(user: any): void {
+    window.sessionStorage.removeItem('USER');
+    window.sessionStorage.setItem('USER', JSON.stringify(user));
+  }
 }
